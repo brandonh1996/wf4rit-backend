cefac4f82880d6a1c26616aee21611265da48cb8
jeremy-molder <jem5346@rit.edu>
2020-02-18 14:39:03 -0500

    user API login, create, and update functionality along with JWT validatio

---
+++ api/login.php
@@ -0,0 +1,119 @@
+<?php
+// required headers
+header("Access-Control-Allow-Origin: http://localhost/rest-api-authentication-example/");
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Methods: POST");
+header("Access-Control-Max-Age: 3600");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+ 
+// database connection will be here
+// files needed to connect to database
+include_once 'config/database.php';
+include_once 'objects/user.php';
+ 
+// get database connection
+$database = new Database();
+$db = $database->getConnection();
+ 
+// instantiate user object
+$user = new User($db);
+ 
+// check email existence here// get posted data
+$data = json_decode(file_get_contents("php://input"));
+ 
+// set product property values
+$user->email = $data->email;
+$email_exists = $user->emailExists();
+ 
+// generate json web token
+include_once 'config/core.php';
+include_once 'libs/php-jwt-master/src/BeforeValidException.php';
+include_once 'libs/php-jwt-master/src/ExpiredException.php';
+include_once 'libs/php-jwt-master/src/SignatureInvalidException.php';
+include_once 'libs/php-jwt-master/src/JWT.php';
+use \Firebase\JWT\JWT;
+ 
+// check if email exists and if password is correct
+if($email_exists && password_verify($data->password, $user->password)){
+ 
+    $token = array(
+       "iss" => $iss, //issuer -->identifies the principle that issued JWT
+       "aud" => $aud, //audience --> intended recepient of JWT 
+       "iat" => $iat, //issued at --> time which the JWT was issued
+       "nbf" => $nbf, //not before --> time before the JWT must NOT be accepted for processing
+       "data" => array(
+           "id" => $user->id,
+           "firstname" => $user->firstname,
+           "lastname" => $user->lastname,
+           "email" => $user->email
+       )
+    );
+ 
+    // set response code
+    http_response_code(200);
+ 
+    // generate jwt
+    $jwt = JWT::encode($token, $key);
+    echo json_encode(
+            array(
+                "message" => "Successful login.",
+                "jwt" => $jwt
+            )
+        );
+ 
+}
+ 
+// login failed
+else{
+ 
+    // set response code
+    http_response_code(401);
+ 
+    // tell the user login failed
+    echo json_encode(array("message" => "Login failed."));
+}
+// check if given email exist in the database
+function emailExists(){
+ 
+    // query to check if email exists
+    $query = "SELECT id, firstname, lastname, password
+            FROM " . $this->user . "
+            WHERE email = ?
+            LIMIT 0,1";
+ 
+    // prepare the query
+    $stmt = $this->conn->prepare( $query );
+ 
+    // sanitize
+    $this->email=htmlspecialchars(strip_tags($this->email));
+ 
+    // bind given email value
+    $stmt->bindParam(1, $this->email);
+ 
+    // execute the query
+    $stmt->execute();
+ 
+    // get number of rows
+    $num = $stmt->rowCount();
+ 
+    // if email exists, assign values to object properties for easy access and use for php sessions
+    if($num>0){
+ 
+        // get record details / values
+        $row = $stmt->fetch(PDO::FETCH_ASSOC);
+ 
+        // assign values to object properties
+        $this->id = $row['id'];
+        $this->firstname = $row['firstname'];
+        $this->lastname = $row['lastname'];
+        $this->password = $row['password'];
+ 
+        // return true because email exists in the database
+        return true;
+    }
+ 
+    // return false if email does not exist in the database
+    return false;
+}
+ 
+// update() method will be here
---
+++ objects/user.php
@@ -0,0 +1,105 @@
+<?php
+// 'user' object
+class User{
+ 
+    // database connection and table name
+    private $conn;
+    private $table_name = "users";
+ 
+    // object properties
+    public $id;
+    public $firstname;
+    public $lastname;
+    public $email;
+    public $password;
+ 
+    // constructor
+    public function __construct($db){
+        $this->conn = $db;
+    }
+ 
+// create() method will be here
+}
+// create new user record
+function create(){
+    // isActive true
+    // company
+
+ 
+    // insert query
+    $query = "INSERT INTO " . $this->user . "
+            SET
+                first_name = :firstname,
+                last_name = :lastname,
+                email_address = :email,
+                password = :password";
+ 
+    // prepare the query
+    $stmt = $this->conn->prepare($query);
+ 
+    // sanitize
+    $this->firstname=htmlspecialchars(strip_tags($this->firstname));
+    $this->lastname=htmlspecialchars(strip_tags($this->lastname));
+    $this->email=htmlspecialchars(strip_tags($this->email));
+    $this->password=htmlspecialchars(strip_tags($this->password));
+ 
+    // bind the values
+    $stmt->bindParam(':firstname', $this->firstname);
+    $stmt->bindParam(':lastname', $this->lastname);
+    $stmt->bindParam(':email', $this->email);
+    $stmt->bindParam(':password', $this->password);
+ 
+    // execute the query, also check if query was successful
+    if($stmt->execute()){
+        return true;
+    }
+ 
+    return false;
+}
+
+// update a user record
+function update(){
+ 
+    // if password needs to be updated
+    $password_set=!empty($this->password) ? ", password = :password" : "";
+ 
+    // if no posted password, do not update the password
+    $query = "UPDATE " . $this->table_name . "
+            SET
+                firstname = :firstname,
+                lastname = :lastname,
+                email = :email
+                {$password_set}
+            WHERE id = :id";
+ 
+    // prepare the query
+    $stmt = $this->conn->prepare($query);
+ 
+    // sanitize
+    $this->firstname=htmlspecialchars(strip_tags($this->firstname));
+    $this->lastname=htmlspecialchars(strip_tags($this->lastname));
+    $this->email=htmlspecialchars(strip_tags($this->email));
+ 
+    // bind the values from the form
+    $stmt->bindParam(':firstname', $this->firstname);
+    $stmt->bindParam(':lastname', $this->lastname);
+    $stmt->bindParam(':email', $this->email);
+ 
+    // hash the password before saving to database
+    if(!empty($this->password)){
+        $this->password=htmlspecialchars(strip_tags($this->password));
+        $stmt->bindParam(':password', $password);
+    }
+ 
+    // unique ID of record to be edited
+    $stmt->bindParam(':id', $this->id);
+ 
+    // execute the query
+    if($stmt->execute()){
+        return true;
+    }
+ 
+    return false;
+}
+ 
+// emailExists() method will be here
